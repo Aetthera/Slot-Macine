27056a8438fc765abe472f1663b1f5e848899279
Alena <alenabelovadmvn@gmail.com>
2024-09-19 15:25:27 -0400

    Initial Commit

---
+++ Slot Macine/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Slot Macine
+//
+//  Created by Alena Belova  on 2024-09-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Slot Macine/Slot_MacineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slot_MacineApp.swift
+//  Slot Macine
+//
+//  Created by Alena Belova  on 2024-09-19.
+//
+
+import SwiftUI
+
+@main
+struct Slot_MacineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
