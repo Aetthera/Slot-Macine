a1c9fb78d663c9868f4698fcc86a6a9f4b05f134
Alena <alenabelovadmvn@gmail.com>
2024-09-19 16:03:38 -0400

    Project created

---
+++ Slot Macine/SwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  Slot Macine
+//
+//  Created by Alena Belova  on 2024-09-19.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
+
+
